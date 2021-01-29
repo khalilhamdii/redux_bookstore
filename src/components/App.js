2eb3a5040f6d244e86da23a24ad6821a7a1d9420
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-29 02:15:33 +0100

    Implement app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BooksList from '../containers/Bookslist';
+import BooksForm from '../containers/BooksForm';
+function App() {
+  return (
+    <div>
+      <BooksList />
+      <BooksForm />
+    </div>
+  );
+}
+
+export default App;
